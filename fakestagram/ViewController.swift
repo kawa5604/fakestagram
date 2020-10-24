e8047cde7638c89e6ffbe5b4b55abdf36607c205
Jorge G <jorge.gar@me.com>
2020-10-23 19:25:18 -0700

    Initial Commit

---
+++ fakestagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fakestagram
+//
+//  Created by Jorge Garcia on 10/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
